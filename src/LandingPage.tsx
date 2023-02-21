9ca6991f33ddedf4ef21baef8c162d3cd9adb6cd
z20ist25 <z20ist25@z00p3-01.SPSKNM>
2023-02-21 09:53:24 +0100

    New boxes for points and logo

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -51,14 +51,29 @@ export default function LandingPage( userData ): JSX.Element {
 
     return (
         <div className="h-full w-full bg-[url('/src/assets/bg.png')]">
-            <div className="flex items-center pl-5">
-                <img src="src/assets/logo.svg" width="150" height="150" className="rounded-full" alt="obrazok"></img>
-                <a className="text-4xl text-white">PowaCraft</a>
-                <a className="text-xl align-bottom text-amber-400">Admin</a>
-                <div className="place-content-end flex absolute right-10 w-52 h-20 items-stretch">
-                <button className="text-2xl text-white text-center"flex-end onClick={handleLogOut}>Odhlásiť</button>
-                <div className="flex w-5"></div>
-                <img src="src/assets/steve.png" className="w-20 h-20 rounded-full"></img>
+            <div className="flex items-center pl-5 w-full ">
+                <div className="flex items-center w-8/12 ">
+                    <img src="src/assets/logo.svg" width="150" height="150" className="rounded-full" alt="obrazok"></img>
+                    <a className="text-4xl text-white">PowaCraft</a>
+                    <div className="h-14 flex items-end">
+                        <a className="text-xl text-amber-400">Admin</a>
+                    </div>
+                </div>
+                <div className=" place-content-end flex w-2/12 h-20 items-center space-x-6">
+                    <div className="bg-white rounded-full flex justify-center items-center w-52 h-14 ">
+                        <a className="text-2xl">Tvoje body:</a>
+                    </div>
+                    <div className="bg-white rounded-full flex justify-center items-center w-16 h-14">
+                        <a className="text-green-500 text-2xl">+8</a> 
+                    </div>
+                    <div className="bg-white rounded-full flex justify-center items-center w-16 h-14">
+                        <a className="text-red-500 text-2xl">-2</a>
+                    </div>    
+                </div>
+                <div className="place-content-end flex justify-end w-2/12 h-20 items-stretch right-10 pr-10">
+                    <button className="text-2xl text-white text-center"flex-end onClick={handleLogOut}>Odhlásiť</button>
+                    <div className="flex w-5"></div>
+                    <img src="src/assets/steve.png" className="w-20 h-20 rounded-full"></img>
                 </div>
             </div>
 
@@ -81,70 +96,72 @@ export default function LandingPage( userData ): JSX.Element {
                         <a className="text-2xl text-white">Tvoje posledné body</a>
                         <hr/>
                     </div>
+                    <div className="sticky">
+                        <div className="w-full flex items-center pb-2">
+                            <div className="box-content h-4 w-8/12 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
+                                <a className="text-xl">Pomoc hráčovi</a>
+                            </div>
+                            <div className="box-content h-4 w-1/6 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
+                                <a className="text-2xl text-green-600">+1</a>
+                            </div>
+                        </div>
 
-                    <div className="w-full flex inline-block pb-2">
-                        <div className="box-content h-4 w-8/12 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
-                            <a className="text-xl">Pomoc hráčovi</a>
+                        <div className="w-full flex items-center pb-2">
+                            <div className="box-content h-4 w-8/12 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
+                                <a className="text-xl">Nahlásenie hráča</a>
+                            </div>
+                            <div className="box-content h-4 w-1/6 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
+                                <a className="text-2xl text-green-600">+2</a>
+                            </div>
                         </div>
-                        <div className="box-content h-4 w-1/6 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
-                            <a className="text-2xl text-green-600">+1</a>
-                        </div>
-                    </div>
 
-                    <div className="w-full flex inline-block pb-2">
-                        <div className="box-content h-4 w-8/12 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
-                            <a className="text-xl">Nahlásenie hráča</a>
+                        <div className="w-full flex items-center">
+                            <div className="box-content h-4 w-8/12 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
+                                <a className="text-xl">Porušenie pravidiel</a>
+                            </div>
+                            <div className="box-content h-4 w-1/6 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
+                                <a className="text-2xl text-red-600">-1</a>
+                            </div>
                         </div>
-                        <div className="box-content h-4 w-1/6 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
-                            <a className="text-2xl text-green-600">+2</a>
+                        
                         </div>
-                    </div>
-
-                    <div className="w-full flex inline-block">
-                        <div className="box-content h-4 w-8/12 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
-                            <a className="text-xl">Porušenie pravidiel</a>
-                        </div>
-                        <div className="box-content h-4 w-1/6 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
-                            <a className="text-2xl text-red-600">-1</a>
-                        </div>
-                    </div>
-                </div>     
+                    </div>     
 
 
-                <div className="box-content bg-zinc-700/80 rounded-lg mb-10 p-2 pb-7">
+                    <div className="box-content bg-zinc-700/80 rounded-lg mb-10 p-2 pb-7">
 
-                    <div className="justify-center flex pb-3 pt-2 bg-amber-500/40 rounded-lg mb-2">
-                        <a className="text-2xl text-white">Najlepší helperi</a>
-                        <hr/>
-                    </div>
+                        <div className="justify-center flex pb-3 pt-2 bg-amber-500/40 rounded-lg mb-2">
+                            <a className="text-2xl text-white">Najlepší helperi</a>
+                            <hr/>
+                        </div>
 
-                    <div className="w-full flex inline-block pb-2">
-                        <div className="box-content h-4 w-8/12 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
-                            <a className="text-xl">Hrac1</a>
+                        <div className="w-full flex inline-block pb-2">
+                            <div className="box-content h-4 w-8/12 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
+                                <a className="text-xl">Hrac1</a>
+                            </div>
+                            <div className="box-content h-4 w-1/6 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
+                                <a className="text-2xl text-green-600">+23</a>
+                            </div>
                         </div>
-                        <div className="box-content h-4 w-1/6 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
-                            <a className="text-2xl text-green-600">+23</a>
-                        </div>
-                    </div>
 
-                    <div className="w-full flex inline-block pb-2">
-                        <div className="box-content h-4 w-8/12 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
-                            <a className="text-xl">Hrac2</a>
+                        <div className="w-full flex inline-block pb-2">
+                            <div className="box-content h-4 w-8/12 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
+                                <a className="text-xl">Hrac2</a>
+                            </div>
+                            <div className="box-content h-4 w-1/6 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
+                                <a className="text-2xl text-green-600">+16</a>
+                            </div>
                         </div>
-                        <div className="box-content h-4 w-1/6 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
-                            <a className="text-2xl text-green-600">+16</a>
-                        </div>
-                    </div>
 
-                    <div className="w-full flex inline-block">
-                        <div className="box-content h-4 w-8/12 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
-                            <a className="text-xl">Hrac3</a>
+                        <div className="w-full flex inline-block">
+                            <div className="box-content h-4 w-8/12 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
+                                <a className="text-xl">Hrac3</a>
+                            </div>
+                            <div className="box-content h-4 w-1/6 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
+                                <a className="text-2xl text-green-600">+9</a>
+                            </div>
                         </div>
-                        <div className="box-content h-4 w-1/6 p-4 bg-white rounded-lg mx-2 items-center justify-center flex">
-                            <a className="text-2xl text-green-600">+9</a>
-                        </div>
-                    </div>
-                </div>       
+                    </div>       
             </div>   
 
 
@@ -166,7 +183,7 @@ export default function LandingPage( userData ): JSX.Element {
                 <div className="box-content h-1/2 w-3/6 bg-zinc-700/80 rounded-lg">
 
                     <div className="justify-center flex pb-3 pt-2">
-                        <a className="text-4xl text-white">Zoznam úloh</a>
+                        <a className="font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-yellow-500 to-lime-600">Zoznam úloh</a>
                         <hr/>
                     </div>
 
