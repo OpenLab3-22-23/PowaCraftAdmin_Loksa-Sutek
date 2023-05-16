8372470619dd39a7fb16b2770b69afce79e0fc75
z20ist13 <z20ist13@z00p3-02.SPSKNM>
2023-05-16 09:54:29 +0200

    Finished member panel and started adding account deleting (OnwerPanel)

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
   <div className="w-screen h-screen">
     {session ? (
-      <LandingPage userData={session}/> 
+      <OwnerPanel userData={session}/> 
     ) : <Navigate to="/login" />}
   </div>
 
--- src/userpanel/OwnerATList.tsx
+++ src/userpanel/OwnerATList.tsx
@@ -1,9 +1,10 @@
 import WriteUserRank from "./UserRank";
-import { useState} from "react";
+import { useState } from "react";
 import { supabase } from "../supabase/supabaseClient";
 
 export default function OwnerATList({response, onRefresh}) {
         
+    //const [hasPoints, setHasPoints] = useState(false);    
 
     const addPoint = async (user) => {
         const { error } = await supabase
@@ -32,24 +33,42 @@ export default function OwnerATList({response, onRefresh}) {
         {
             response.map((user) => 
 
-            <div className="w-full flex inline-block pb-5 items-center">
-                <div className="items-center flex bg-white rounded-2xl box-content h-12 w-full w-9/12 ">
+            <div className="w-full flex inline-block pb-5 items-center justify-center">
+
+                    {(user.rank == "Akademik" || user.rank == "Helper") &&
+                        <button onClick={() => addPoint(user)} className="box-content h-4 w-max p-4 bg-gray-600 border-slate-500 border-2 text-green-600 rounded-lg mx-6 items-center flex justify-center">+1</button>
+                    }
+            
+
+                <div className="items-center flex bg-white rounded-2xl box-content h-12 justify-center w-2/6 mx-2">
                     <div className="mx-6">
                         <a className="text-2xl text-black">{user.username}</a>
                     </div>
+                </div>
+
+
+                <div className="items-center flex bg-white rounded-2xl box-content h-12 justify-center w-1/5 mx-2">
                     <div className="mx-6 text-2xl">
                         <WriteUserRank rank={user.rank}/>
                     </div>
+                </div>
+
+                <div className="items-center flex bg-white rounded-2xl box-content h-12 justify-center w-1/12 mx-2">
                     <div className="mx-6">
                         <a className="text-green-600 text-2xl">+{user.plus}</a>
                     </div>
+                </div>
+
+                <div className="items-center flex bg-white rounded-2xl box-content h-12 justify-center w-1/12 mx-2">
                     <div className="mx-6">
                         <a className="text-red-600 text-2xl">-{user.minus}</a>
                     </div>
-                </div>    
-                
-                <button onClick={() => addPoint(user)} className="box-content h-4 w-1/12 p-4 bg-gray-600 border-slate-500 border-2 text-green-600 rounded-lg mx-5 items-center flex justify-center">+1</button>
-                <button onClick={() => removePoint(user)} className="box-content h-4 w-1/12 p-4 bg-gray-600 border-slate-500 border-2 text-red-600 rounded-lg mx-5 items-center flex justify-center">-1</button>
+                </div>
+
+                    {(user.rank == "Akademik" || user.rank == "Helper") &&
+                        <button onClick={() => removePoint(user)} className="box-content h-4 w-max p-4 bg-gray-600 border-slate-500 border-2 text-red-600 rounded-lg mx-8 items-center flex justify-center">-1</button>
+                    }
+
             </div>
         )}
         </div>
