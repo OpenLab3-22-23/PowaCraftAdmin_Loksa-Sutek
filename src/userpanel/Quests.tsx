d7bbc45e10a6b87cdde463610a1c305aa3507917
Psychoman500 <martin.sutek@gmail.com>
2023-11-29 20:56:01 +0100

    some small changes

--- src/userpanel/Quests.tsx
+++ src/userpanel/Quests.tsx
@@ -19,13 +19,13 @@ export default function WriteQuests({questList, deleteShown, onDelete}) {
         {
             questList.map((quest, index) => 
             <div key={index} className="w-full flex inline-block pb-5 items-center">
-                <div className="box-content h-8 w-1/12 p-4 bg-white rounded-lg mx-8 items-center justify-center flex">
+                <div className="box-content h-8 w-1/12 p-4 bg-white rounded-lg mx-4 items-center justify-center flex">
                     <a className="text-2xl">{++index}.</a> 
                 </div>
-                <div className="box-content h-full w-9/12 lg:p-4 px-4 bg-zinc-500/40 rounded-lg flex overflow-auto lg:overflow-hidden">
+                <div className="box-content h-8 w-9/12 lg:p-4 px-4 pb-8 bg-zinc-500/40 rounded-lg flex overflow-auto lg:overflow-hidden">
                         <a className="text-2xl text-white flex">{quest.quest_name}</a>
                 </div>
-                <div className="box-content h-8 w-1/12 p-4 bg-white rounded-lg mx-8 items-center justify-center flex">
+                <div className="box-content h-8 w-1/12 p-4 bg-white rounded-lg mx-4 items-center justify-center flex">
                         <a className="text-green-600 text-2xl">+{quest.points}</a>
                 </div>
                 <button onClick={() => deleteQuest(quest.id) } style={{ display: deleteShown ? "block" : "none" }} className="bg-red-500 hover:bg-red-500/70 w-12 h-12 rounded-lg text-white mr-8 text-2xl hidden">X</button>
