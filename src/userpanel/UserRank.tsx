07e28d26309e8bd28c1c898f44582772d672ad13
Lukáš Lokša <86780297+Powissen@users.noreply.github.com>
2024-01-26 18:19:46 +0100

    Rename test variable in UserRank.tsx

--- src/userpanel/UserRank.tsx
+++ src/userpanel/UserRank.tsx
@@ -4,10 +4,10 @@ export default function WriteUserRank({rank, rankList})
 {
     if (rankList != null)
     {
-        const result = rankList.map((ranq, index) => {
-            if (rank == ranq.rank)
+        const result = rankList.map((rankFromRankList, index) => {
+            if (rank == rankFromRankList)
             {
-                return <a key={index} className={`text-4xl`} style={{ color: ranq.colour }}>{rank}</a>
+                return <a key={index} className={`text-4xl`} style={{ color: rankFromRankList.colour }}>{rank}</a>
             }
         });
         return result.length > 0 ? result : <a className="text-gray-500 text-4xl">UNKNOWN</a>;
